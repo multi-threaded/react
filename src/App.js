ff47e4fd4cbf732c0129e7af064a48d962940de1
multi-threading <mjagoyer@gmail.com>
2022-11-16 11:46:45 -0500

    created navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import AddEmployee from "./components/AddEmployee";
 import EditEmployee from "./components/EditEmployee";
+import Header from "./components/Header";
 
 function App() {
   const [role, setRole] = useState("Developer");
@@ -48,7 +49,7 @@ function App() {
 
   function updateEmployee(id, newName, newRole) {
     const updatedEmployees = employees.map((employee) => {
-      if (id == employee.id) {
+      if (id === employee.id) {
         return { ...employee, name: newName, role: newRole };
       }
       return employee;
@@ -69,16 +70,11 @@ function App() {
   const showEmployees = true;
 
   return (
-    <div className="App">
+    <div className="App bg-gray-300 min-h-screen">
+      <Header />
       {showEmployees ? (
         <>
-          <input
-            type="text"
-            onChange={(e) => {
-              setRole(e.target.value);
-            }}
-          ></input>
-          <div className="flex flex-wrap justify-center">
+          <div className="flex flex-wrap justify-center my-2">
             {employees.map((employee) => {
               const editEmployee = (
                 <EditEmployee
